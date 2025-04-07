7b625589afc782826f58f9d52ecedfed22ec15a3
ErwannRousseau <erwann.rousseau@icloud.com>
2025-04-07 12:52:18 +0200

    feat: update eslint configuration and add react plugin

--- apps/frontend/eslint.config.mjs
+++ apps/frontend/eslint.config.mjs
@@ -3,9 +3,9 @@ import nextPlugin from '@next/eslint-plugin-next'
 import stylistic from '@stylistic/eslint-plugin'
 import jsxA11y from 'eslint-plugin-jsx-a11y'
 import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
+import reactPlugin from 'eslint-plugin-react'
 
 export default tseslint.config(
   {
@@ -13,19 +13,22 @@ export default tseslint.config(
       js.configs.recommended,
       ...tseslint.configs.recommended,
       jsxA11y.flatConfigs.recommended,
-      reactRefresh.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      reactPlugin.configs.flat.recommended,
+      reactPlugin.configs.flat['jsx-runtime'],
     ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
+      ...reactPlugin.configs.flat.recommended.languageOptions,
       globals: { ...globals.browser, ...globals.node },
     },
     plugins: {
       '@next/next': nextPlugin,
+      'react-hooks': reactHooks,
     },
     rules: {
       ...nextPlugin.configs.recommended.rules,
       ...nextPlugin.configs['core-web-vitals'].rules,
+      ...reactHooks.configs.recommended.rules,
     },
   },
   // Style/Formatting
@@ -38,5 +41,5 @@ export default tseslint.config(
       '@stylistic/jsx-one-expression-per-line': ['error', { allow: 'non-jsx' }],
     },
   },
-  { ignores: ['.next'] },
+  { ignores: ['.next', 'public'] },
 )
