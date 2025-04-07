1a830cbc9992e5ba06af77e2b1c67e2140406ad0
ErwannRousseau <erwann.rousseau@icloud.com>
2025-04-07 12:57:01 +0200

    fix/style: ✨

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,10 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-	output: "standalone",
-	eslint: {
-		dirs: ["<rootDir>"],
-	},
-};
+  output: 'standalone',
+  eslint: {
+    dirs: ['<rootDir>'],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
