861fa8144b7d2c936744e6376f98bbfdd642e943
Powerm1nt <leonard.chabot@nuka.works>
2025-04-07 17:27:12 +0200

    backend::app: Fixed imports

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AuthModule } from './auth/modules/auth.module';
-import { CoursesModule } from './modules/courses.module';
+import { CoursesModule } from './courses/modules/courses.module';
 
 @Module({
   imports: [AuthModule, CoursesModule],
-  controllers: [AppController],
+  controllers: [],
   providers: [],
 })
 export class AppModule {} 
