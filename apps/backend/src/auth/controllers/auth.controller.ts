d4eb29b910592f266bceee40e8960482fbde01d1
Mickael <campos.mickael66@gmail.com>
2025-04-08 10:56:59 +0200

    :lock: Backend - Auth : Validating dto before use in service

--- apps/backend/src/auth/controllers/auth.controller.ts
+++ apps/backend/src/auth/controllers/auth.controller.ts
@@ -1,15 +1,21 @@
-import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Inject, Post } from '@nestjs/common';
 import { RegisterDto } from '../dto/register.dto';
 import { AuthService } from '../services/auth.service';
+import { validate } from 'class-validator';
 
 @Controller('auth')
 export class AuthController {
-  constructor(
-      @Inject() private authService: AuthService,
-    ) {}
+    constructor(
+        @Inject() private authService: AuthService,
+    ) { }
 
-  @Post('signup')
-  register(@Body() dto: RegisterDto) {
-    return this.authService.register(dto);
-  }
+    @Post('signup')
+    register(@Body() dto: RegisterDto) {
+        validate(dto).then(errors => {
+            if (errors.length > 0) {
+                throw new BadRequestException('Validation failed');
+            }
+        });
+        return this.authService.register(dto);
+    }
 }
