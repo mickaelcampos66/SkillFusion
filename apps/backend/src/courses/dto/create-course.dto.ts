35932240ef18eb4a4a4543a74da1db413c95ae21
r2 <121185412+ErwannRousseau@users.noreply.github.com>
2025-04-10 16:46:13 +0200

    fix: type error on update and create course dto (#23)
    Add unique constraint on Course name and set default timestamps
    
    - Added a unique constraint on the `name` column of the `Course` table.
    - Made `created_at` and `updated_at` columns required for the `Course`, `Message`, `Post`, `Category`, `Role`, and `User` tables.
    - Set default values for `created_at` and `updated_at` columns to the current timestamp for the affected tables.

--- apps/backend/src/courses/dto/create-course.dto.ts
+++ apps/backend/src/courses/dto/create-course.dto.ts
@@ -1,4 +1,30 @@
-import { OmitType } from '@nestjs/mapped-types';
-import { CourseDto } from './course.dto';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
-export class CreateCourseDto extends OmitType(CourseDto, ['id']) {}
+export class CreateCourseDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsString()
+  @IsNotEmpty()
+  content: string;
+
+  @IsString()
+  image: string | null;
+
+  @IsString()
+  @IsNotEmpty()
+  level: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  created_by: number;
+
+  // @IsArray()
+  // @IsString({ each: true })
+  // courseCategories: string[];
+}
