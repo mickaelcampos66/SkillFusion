64a22ed5ebbd2b75bee697ae26716686f06292df
Leonard Chabot <leonard.chabot@nuka.works>
2025-04-29 13:55:10 +0000

    Backend::HealthChk: Added simple healthcheck module (#29)
    * Backend::HealthChk: Added simple healthcheck module
    
    * Update apps/backend/src/healthchk/controllers/healthchk.controller.ts
    
    Co-authored-by: r2 <121185412+ErwannRousseau@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: r2 <121185412+ErwannRousseau@users.noreply.github.com>

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/modules/auth.module';
 import { UserModule } from './user/modules/user.module';
 import { CoursesModule } from './courses/modules/courses.module';
+import { HealthChkModule } from './healthchk/modules/healthchk.module';
 
 @Module({
-  imports: [AuthModule, UserModule, CoursesModule],
+  imports: [AuthModule, UserModule, CoursesModule, HealthChkModule],
   controllers: [],
   providers: [],
 })
