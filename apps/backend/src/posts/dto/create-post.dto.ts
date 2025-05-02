614108469c0e26c4a491394c07eb61ed29b8c4c2
Mika wsl Laptop <campos.mickael66@gmail.com>
2025-05-02 11:23:54 +0200

    :construction: Backend - adding create post dto

--- apps/backend/src/posts/dto/create-post.dto.ts
+++ apps/backend/src/posts/dto/create-post.dto.ts
@@ -1,1 +1,35 @@
-export class CreatePostDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsDate, IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+export class CreatePostDto {
+    @ApiProperty({
+        description: 'The title of the post',
+        example: 'This is a sample post title.',
+    })
+    @IsString()
+    @IsNotEmpty()
+    title: string
+
+    @ApiProperty({
+        description: 'The content of the post',
+        example: 'This is a sample post content.',
+    })
+    @IsString()
+    @IsNotEmpty()
+    content: string
+
+    @ApiProperty({
+        description: 'The author of the post',
+        example: 1,
+    })
+    @IsNumber()
+    @IsNotEmpty()
+    user_id: number
+
+    @IsDate()
+    @ApiProperty({
+        description: 'The date when the post was created',
+        example: '2023-01-01T00:00:00Z',
+    })
+    created_at: Date
+}
