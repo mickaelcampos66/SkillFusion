7b0d4f55f7d8419200d22d8b4744d6b95df29b38
r2 <121185412+ErwannRousseau@users.noreply.github.com>
2025-05-22 13:34:47 +0200

    style(dashboard): fix grid (#53)

--- apps/frontend/src/app/(dashboard)/dashboard/my-courses/page.tsx
+++ apps/frontend/src/app/(dashboard)/dashboard/my-courses/page.tsx
@@ -26,7 +26,7 @@ export default async function Page() {
   return (
     <SidebarInset>
       <Header pages={['Mes cours']} />
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center p-4">
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(280px,1fr))] gap-6 justify-items-center p-4">
         {courses.map(course => (
           <CourseCard key={course.id} {...course} link={`/dashboard/my-courses/${course.id}`} />
         ))}
